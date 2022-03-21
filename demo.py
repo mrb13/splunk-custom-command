3047cd734ee3efe98aa60a5dba89c4031f914c07
Marcus B <3343976+mrb13@users.noreply.github.com>
2022-03-21 14:27:39 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,22 +2,20 @@ import requests
 import sys
 import json
 sys.path.append('/opt/splunk/lib/python3.7')
-sys.path.append('/opt/splunk/lib/python3.7/site-packages/splunk')
+sys.path.append('/opt/splunk/lib/python3.7/site-packages/splunk') 
 sys.path.append('/opt/splunk/lib/python2.7')
 sys.path.append('/opt/splunk/lib/python2.7/site-packages/splunk')
 
 import Intersplunk as man
 
+x1 = sys.argv[1]
+xx = int(x1)
+#print(x1)
+#print(xx)
 response =requests.get("https://randomuser.me/api/")
-
-
 dataa = json.loads(response.text)
 #print(dataa["results"][0]["name"])
 result = []
-result.append(dataa["results"][0]["name"])
+result.append(dataa["results"][xx]["name"])
 
 man.outputResults(result)
-
-
-
- 
